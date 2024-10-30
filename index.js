84c9255390cccbb6b8fea520837e5efd13ceb58b
misbah-aslam16 <misbahaslam000@gmail.com>
2024-10-30 08:27:31 +0500

    new

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 //====================QUESTION 01====================//
+function showAlert() {
+    alert("You clicked the link!");
+}
+
+//====================QUESTION 02====================//
+
+function showAlert(mobileName) {
+    alert("You clicked on " + mobileName);
+}
+
+//====================QUESTION 03====================//
+function deleteRow(button) {
+    
+    let row = button.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
+//====================QUESTION 04====================//
+function changeImage() {
+    document.getElementById("hoverImage").src = "./images/mobile4.webp"; // Change to second image
+}
+function resetImage() {
+    document.getElementById("hoverImage").src = "./images/mobile3.webp"; // Revert to first image
+}
+
+
+//====================QUESTION 05====================//
+
+let counter = 0;
+
+        function increaseCounter() {
+            counter++;
+            document.getElementById("counterValue").innerText = counter;
+        }
+
+        function decreaseCounter() {
+            counter--;
+            document.getElementById("counterValue").innerText = counter;
+        }
